4a87a0f092250e657d42f64f2b90c6971a9f1acf
Sam Hinshaw <samuel.hinshaw@gmail.com>
2020-05-18 20:54:10 -0700

    update param example

--- parameter-properties.ts
+++ parameter-properties.ts
@@ -1,20 +1,49 @@
 export class SuperService {
-  constructor(private importantValue: string) {}
+  constructor(private privateValue: string) {}
 
-  public getImportantValue(): string {
-    return this.importantValue;
+  public getValue(): string {
+    return this.privateValue;
   }
 }
+const superService = new SuperService('apple pie')
+console.log(superService.privateValue)
 
 export class VerboseService {
-  private importantValue: string
-  constructor(importantValue: string) {
-    this.importantValue = importantValue;
+  private privateValue: string
+  constructor(privateValue: string) {
+    this.privateValue = privateValue;
   }
   
-  public getImportantValue(): string {
-    return this.importantValue;
+  public getValue(): string {
+    return this.privateValue;
+  }
+}
+const verboseService = new VerboseService('apple pie')
+console.log(verboseService.privateValue)
+
+// this also works for public fields
+
+export class SuperServiceTwo {
+  constructor(public publicValue: string) {}
+
+  public getValue(): string {
+    return this.publicValue;
   }
 }
 
-// likewise with public values
+const superServiceTwo = new SuperServiceTwo('apple pie')
+console.log(superServiceTwo.publicValue)
+
+export class VerboseServiceTwo {
+  public publicValue: string
+  constructor(publicValue: string) {
+    this.publicValue = publicValue;
+  }
+  
+  public getValue(): string {
+    return this.publicValue;
+  }
+}
+
+const verboseServiceTwo = new VerboseServiceTwo('apple pie')
+console.log(verboseServiceTwo.publicValue)
